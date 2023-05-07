1cb4be0304bc19cc6f08b0e33c4fcda2f973c947
Marc Gaston-Johnston <mgastonjohnston@pingtt.com>
2023-05-07 14:47:35 -0400

    Removed old module

--- l10n_tt_hr_payroll/models/hr_rule_parameter.py
+++
@@ -1,6 +0,0 @@
-from odoo import api, fields, models
-
-class HrRuleParameter(models.Model):
-    _inherit = "hr.rule.parameter"
-
-    tax_deduction_type_id = fields.Many2one(comodel_name="tax.deduction.type")
